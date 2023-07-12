e82292733a88249ad0392ea81905196d2a6f5162
meseret1918 <meseret1918@gmail.com>
2023-07-12 10:33:20 -0700

    deploy_web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
+All files in the folder web_static must be added to the final archive
+All archives must be stored in the folder versions
+(your function should create this folder if it doesn't exist)
+The name of the archive created must be
+web_static_<year><month><day><hour><minute><second>.tgz
+The function do_pack must return the archive path
+if the archive has been correctly generated
 
-from datetime import datetime
-from fabric.api import local
-from os.path import isdir
+Otherwise, it should return None
+"""
+from fabric.api import *
+import time
+from datetime import date
 
 
 def do_pack():
-    """generates a tgz archive"""
+    timestamp = time.strftime("%Y%m%d%H%M%S")
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              format(timestamp))
+        return ("versions/web_static_{:s}.tgz".format(timestamp))
     except:
         return None
