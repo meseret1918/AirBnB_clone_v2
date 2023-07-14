2f1cf761153f456430ff0ab56cfb71456648b280
meseret1918 <meseret1918@gmail.com>
2023-07-14 09:00:05 -0700

    AirBnB clone - Deploy static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -73,7 +73,7 @@ file { '/var/www/html':
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
+  content => "hello world test Nginx\n"
 } ->
 
 file { '/var/www/html/404.html':
