d384e0e2bdf581dc9cb98f407fe984127ffb044c
meseret1918 <meseret1918@gmail.com>
2023-07-14 06:37:34 -0700

    Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,12 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# a script that sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install -y nginx
 
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hello World test" > /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared / /data/web_static/releases/test/
+echo "hello world test" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-enabled/default
-
-service nginx restart
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
