dd228b7681bdd9e3eda7424113768c6115460db5
meseret1918 <meseret1918@gmail.com>
2023-07-22 06:03:14 -0700

    clone_v3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,10 +2,6 @@
 """ a Fabric script (based on the file 1-pack_web_static.py) that distributes..
     ..an archive to your web servers, using the function do_deploy: """
 
-
-from fabric.api import *
-from datetime import datetime
-
 from fabric.api import env, put, run
 from os.path import exists
 
