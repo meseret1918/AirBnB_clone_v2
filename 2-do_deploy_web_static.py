8d5d7a6a2f720e684537596c2920d658ede76c00
meseret1918 <meseret1918@gmail.com>
2023-07-22 05:11:36 -0700

    clone_v3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,34 @@
-#!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
-
-from fabric.api import put, run, env
+from fabric.api import env, put, run
 from os.path import exists
+
+# Define the web server IP addresses
 env.hosts = ['52.201.222.41','174.129.54.185']
 
-
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    # Check if the archive file exists
+    if not exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+
+        # Extract the archive to the /data/web_static/releases/<archive filename without extension> directory
+        archive_filename = archive_path.split('/')[-1]
+        archive_folder = '/data/web_static/releases/{}'.format(archive_filename.split('.')[0])
+        run('mkdir -p {}'.format(archive_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, archive_folder))
+
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Delete the symbolic link /data/web_static/current from the web server
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+
+        # Create a new symbolic link /data/web_static/current linked to the new version of your code
+        run('ln -s {} /data/web_static/current'.format(archive_folder))
+
         return True
     except:
         return False
+
