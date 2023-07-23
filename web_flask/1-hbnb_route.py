52194bbc3de017fc5641ba2c2c9138c4088ea18c
meseret1918 <meseret1918@gmail.com>
2023-07-23 07:38:52 -0700

    clone

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application HBNB"""
+"""script that starts a Flask web application"""
+
+
+# import Flask class from flask module
 from flask import Flask
 
+# create an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
-def hello():
+
+@app.route('/')
+def index():
+    """display "Hello HBNB!"
+
+    Returns:
+        str: text on the index page
+    """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+
+@app.route('/hbnb')
+def hbnb_route():
+    """display "HBNB"
+
+    Returns:
+        str: text on the page
+    """
     return 'HBNB'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
 
+if __name__ == '__main__':
+    app.run(debug=True)
