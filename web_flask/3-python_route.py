478a5ee674488c83b94e7483ec7439cc4c0fb4c9
meseret1918 <meseret1918@gmail.com>
2023-07-23 07:41:59 -0700

    clone

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,26 +1,61 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application C is FUN"""
+"""script that starts a Flask web application"""
+
+
+# import Flask class from flask module
 from flask import Flask
 
+# create an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
-def hello():
+
+@app.route('/')
+def index():
+    """display "Hello HBNB!"
+
+    Returns:
+        str: text on the index page
+    """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+
+@app.route('/hbnb')
+def hbnb_route():
+    """display "HBNB"
+
+    Returns:
+        str: text on the page
+    """
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
+
+@app.route('/c/<text>')
+def c_route(text):
+    """display "C", followed by the value of the text variable
+
+    Args:
+        text (str): text to be served on the page
+
+    Returns:
+        str: text on the page
+    """
     return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_route(text):
+    """display "Python", followed by the value of the text variable
+
+    Args:
+        text (str): text to be served on the page
+
+    Returns:
+        str: text on the page
+    """
     return 'Python {}'.format(text.replace('_', ' '))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
 
+if __name__ == '__main__':
+    app.run(debug=True)
