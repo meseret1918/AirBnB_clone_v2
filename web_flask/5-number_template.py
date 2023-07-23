9b36e3f626b1916ca9bd964975200fc8ac800e30
meseret1918 <meseret1918@gmail.com>
2023-07-23 07:45:08 -0700

    clone

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,34 +1,53 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application C is FUN"""
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello():
+app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''The home page.'''
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def ... hbnb():
+
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page.'''
     return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
+
+@app.route('/python/<text>')
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+    '''The python page.'''
     return 'Python {}'.format(text.replace('_', ' '))
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
+
+@app.route('/number/<int:n>')
+def number_page(n):
+    '''The number page.'''
     return '{} is a number'.format(n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    return render_template('5-number.html', number=n)
+    '''The number_template page.'''
+    ctxt = {
+        'n': n
+    }
+    return render_template('5-number.html', **ctxt)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port='5000')
