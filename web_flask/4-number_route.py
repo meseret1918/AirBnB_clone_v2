1632c27ae945d1d2878c7d35815333925cb2b2a7
meseret1918 <meseret1918@gmail.com>
2023-07-23 07:43:35 -0700

    clone

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,25 +1,74 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application C is FUN"""
+"""script that starts a Flask web application"""
+
+
+# import Flask class from flask module
 from flask import Flask
 
+# create an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
-def hello():
+
+@app.route('/')
+def index():
+    """display "Hello HBNB!"
+
+    Returns:
+        str: text on the index page
+    """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+
+@app.route('/hbnb')
+def hbnb_route():
+    """display "HBNB"
+
+    Returns:
+        str: text on the page
+    """
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
+
+@app.route('/c/<text>')
+def c_route(text):
+    """display "C", followed by the value of the text variable
+
+    Args:
+        text (str): text to be served on the page
+
+    Returns:
+        str: text on the page
+    """
     return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/c/<int:n>', strict_slashes=False)
-def c_number(n):
-    return 'C {}'.format(n)
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_route(text):
+    """display "Python", followed by the value of the text variable
+
+    Args:
+        text (str): text to be served on the page
+
+    Returns:
+        str: text on the page
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number_route(n):
+    """display "n is a number" only if n is an integer
+
+    Args:
+        n (integer): number to be displayed on page
+
+    Returns:
+        str: text on the page
+    """
+    return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(debug=True)
