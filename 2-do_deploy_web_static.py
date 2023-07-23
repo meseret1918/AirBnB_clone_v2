1b251215968720ec2bf86918f6f47e8babf06a0b
meseret1918 <meseret1918@gmail.com>
2023-07-23 11:02:14 -0700

    clone_v3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,49 @@
 #!/usr/bin/python3
-""" 0x03. AirBnB clone - Deploy"""
-from fabric.api import env, put, run
-from os import path
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-env.hosts = ['52.201.222.41', '174.129.54.185']
-env.user = 'ubuntu'
+env.hosts = ["35.174.200.148", "107.20.20.169"]
+
 
 def do_deploy(archive_path):
-    if not path.exists(archive_path) or archive_path is None:
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    f_name = path.basename(archive_path)
-    d_name = f_name.split('.')[0]
-
-    try:
-        put(local_path=archive_path, remote_path='/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(d_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(f_name, d_name))
-        run('rm /tmp/{}'.format(f_name))
-
-        # Delete the existing directories if they exist
-        run('rm -rf /data/web_static/releases/{}/web_static/images'.format(d_name))
-        run('rm -rf /data/web_static/releases/{}/web_static/styles'.format(d_name))
-        run('rm -rf /data/web_static/releases/{}/web_static/versions'.format(d_name))
-
-        run('mv /data/web_static/releases/{}/web_static/* '.format(d_name) +
-            '/data/web_static/releases/{}/'.format(d_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(d_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(d_name))
-
-        print('New version deployed!')
-        return True
-    except Exception as e:
-        print('Error during deployment:', str(e))
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
