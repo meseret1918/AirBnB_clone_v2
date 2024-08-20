64afd87eaf40ec3c2b7a767fd5eea29db70a30b1
meseret1918 <meseret1918@gmail.com>
2024-08-20 16:42:18 +0000

    0x1A

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,24 +1,11 @@
-#!/usr/bin/python3
-"""script that starts a Flask web application"""
-
-
-# import Flask class from flask module
 from flask import Flask
 
-# create an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
+@app.route('/airbnb-onepage/')
+def hello_hbnb():
+    return "Hello HBNB!"
 
-@app.route('/')
-def index():
-    """display "Hello HBNB!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
-    Returns:
-        str: text on the index page
-    """
-    return 'Hello HBNB!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
